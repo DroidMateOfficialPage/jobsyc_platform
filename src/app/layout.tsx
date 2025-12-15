9b18bd71fb0d37ea80f46b395d568c40ab675f34
jobsyc <ivana@jobsyc.co>
2025-12-15 11:16:06 +0100

    Bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,6 +110,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y94Y6MBKJ8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y94Y6MBKJ8');
+          `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
